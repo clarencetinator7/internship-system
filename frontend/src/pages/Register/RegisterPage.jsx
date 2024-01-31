baba89340a0b8752a1da3758adeb6009929e32fe
Clarence Joseph Dimafelix <clarencejoseph.dimafelix@gmail.com>
2024-01-31 16:22:46 +0800

    Refactor register form to use react-hook-form

--- frontend/src/pages/Register/RegisterPage.jsx
+++ frontend/src/pages/Register/RegisterPage.jsx
@@ -1,42 +1,101 @@
+import { useForm } from "react-hook-form";
 import Container from "react-bootstrap/Container";
 import { Form, Row, Col, Button } from "react-bootstrap";
 import styles from "./RegisterPage.module.css";
 
 const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Container className={styles.container}>
       <div className={styles.card}>
         <h1>Register</h1>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="firstName">
+            <Form.Group as={Col} controlId="groupFirstName">
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter first name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter first name"
+                {...register("firstName", { required: true })}
+              />
+              {errors.firstName && (
+                <Form.Text className="text-danger">
+                  First Name is required
+                </Form.Text>
+              )}
             </Form.Group>
-            <Form.Group as={Col} controlId="lastName">
+            <Form.Group as={Col} controlId="groupLastName">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter last name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter last name"
+                {...register("lastName", { required: true })}
+              />
+              {errors.lastName && (
+                <Form.Text className="text-danger">
+                  Last Name is required
+                </Form.Text>
+              )}
             </Form.Group>
           </Row>
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="email">
+            <Form.Group as={Col} controlId="groupEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                {...register("email", {
+                  required: { value: true, message: "Email is required." },
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Entered value does not match email format",
+                  },
+                })}
+              />
+              {errors.email && (
+                <Form.Text className="text-danger">
+                  {errors.email.message}
+                </Form.Text>
+              )}
             </Form.Group>
           </Row>
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="password">
+            <Form.Group as={Col} controlId="groupPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" />
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                {...register("password", { required: true })}
+              />
+              {errors.password && (
+                <Form.Text className="text-danger">
+                  Password is required
+                </Form.Text>
+              )}
             </Form.Group>
           </Row>
           <Row className="mb-3">
-            <Form.Group as={Col} controlId="confirmPassword">
+            <Form.Group as={Col} controlId="groupConfirmPassword">
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="Enter password again"
+                {...register("confirmPassword", { required: true })}
               />
+              {errors.confirmPassword && (
+                <Form.Text className="text-danger">
+                  Password is required
+                </Form.Text>
+              )}
             </Form.Group>
           </Row>
           <Button variant="primary" type="submit">
