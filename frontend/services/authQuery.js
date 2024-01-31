5d3c40d1022095d765365828f44146cc746698fe
Wilhelm Miranda <70352471+WilAm1@users.noreply.github.com>
2024-01-31 17:22:49 +0800

    add: server validation

--- frontend/services/authQuery.js
+++ frontend/services/authQuery.js
@@ -1,10 +1,21 @@
-export const login = async (email, password) => {
-  const { data } = await fetch("/api/auth/login", { email, password });
+const LOGIN_ENDPOINT = "http://127.0.0.1:5000/api/auth/login";
+
+export const login = async ({ identifier, password }) => {
   // send a request.
-  // if the request is successful, check if the response has a cookie
-  // if it has a cookie, set the cookie in the browser
-  // if it doesn't have a cookie, check if the response has an email verification code
-  // if it has an email verification code, redirect to the email verification page
-  // if it doesn't have an email verification code, display the error message
+  const reqBody = {
+    identifier,
+    password,
+  };
+  const res = await fetch(LOGIN_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(reqBody),
+  });
+
+  const data = await res.json();
+
   return data;
 };
--- frontend/src/pages/Login/LoginForm.jsx
+++ frontend/src/pages/Login/LoginForm.jsx
@@ -2,33 +2,70 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useForm } from "react-hook-form";
 
+import { setItem } from "localforage";
+import { login } from "../../../services/authQuery";
+
 function LoginForm() {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setError, // set error message manually, used for server errors
   } = useForm();
 
-  const submitHandler = (data) => {
-    console.log(data);
+  const submitHandler = async (data) => {
+    try {
+      const body = await login(data);
+      console.log(body);
+
+      // check if the response is 422 (input validation error)
+      if (!body?.success) {
+        // get the error message from the response
+        const errorObject = body?.error;
+
+        if (errorObject.identifier)
+          setError("identifier", {
+            type: "server",
+            message: errorObject.identifier,
+          });
+        if (errorObject.password)
+          setError("password", {
+            type: "server",
+            message: errorObject.password,
+          });
+
+        return;
+      }
+
+      // save the token using localforge
+      const token = body.jwt;
+      await setItem("token", token);
+
+      // if it doesn't have a cookie, check if the response has an email verification code
+      // if it has an email verification code, redirect to the email verification page
+      // if it doesn't have an email verification code, display the error message
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <Form noValidate onSubmit={handleSubmit(submitHandler)}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="formBasicIdentifier">
         <Form.Label>Email Address:</Form.Label>
         <Form.Control
-          {...register("email", {
+          {...register("identifier", {
             required: "Email is required",
           })}
-          isInvalid={errors.email} // required if you want to show the error message
           type="email"
+          isInvalid={errors.identifier} // required if you want to show the error message
           placeholder="Enter your Email "
         />
         <Form.Text className="text-muted"></Form.Text>
 
         <Form.Control.Feedback type="invalid">
-          {errors.email?.message}
+          {errors.identifier?.message}
         </Form.Control.Feedback>
       </Form.Group>
 
@@ -41,9 +78,10 @@ function LoginForm() {
             minLength: "Password must be at least 8 characters",
             maxLength: "Password must not exceed 20 characters",
             pattern: {
-              value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,20}$/,
+              // regex for password must contain at least 1 uppercase, 1 lowercase,  1 number, and 1 symbol
+              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
               message:
-                "Password must contain at least 1 uppercase, 1 lowercase, and 1 number",
+                "Password must contain at least 1 uppercase, 1 lowercase,  1 number, and 1 symbol",
             },
           })}
           type="password"
